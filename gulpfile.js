eacd5177352e4db7589ee1fa94614fd47244ec55
Anna <usune@mail.ru>
2022-04-11 03:57:17 +0300

    poidet

--- gulpfile.js
+++ gulpfile.js
@@ -84,7 +84,7 @@ export const scss = () => gulp
     .pipe(gulpif(!dev, autoprefixer(({
         cascade: false,
     }))))
-    .pipe(gulpif(!dev, gcmq()))
+    .pipe(gcmq())
     .pipe(gulpif(!dev, gulp.dest(path.dist.css)))
     .pipe(gulpif(!dev, cleanCSS({
         2: {
--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,10 @@ const book = document.querySelector('.book');
 const add = document.querySelector('.add');
 const addBtns = document.querySelectorAll('.header__btn-add, .library__add-btn');
 const backBtns = document.querySelectorAll('.header__btn_back');
+const btnSearch = document.querySelectorAll('.header__btn_search');
+const search = document.querySelector('.search');
+const btnAdd = document.querySelector('.header__btn-add');
+
 
 const router = new Navigo('/', {
   hash: true,
@@ -14,6 +18,8 @@ const closeAllPage = () => {
   library.classList.add('hidden');
   book.classList.add('hidden');
   add.classList.add('hidden');
+  search.classList.remove('search_active');
+  btnAdd.classList.remove('header__btn-hide');
 }
 
 router.on( {
@@ -42,3 +48,29 @@ backBtns.forEach(btn => {
     router.navigate('/');
   })
 });
+
+const closeSearch = (e) => {
+
+  if (e.target.closest('.search, .header__btn_search')) {
+    return;
+  }
+  search.classList.remove('search_active');
+  btnAdd.classList.remove('header__btn-hide');
+  document.body.removeEventListener('click', closeSearch);
+};
+
+btnSearch.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    search.classList.add('search_active');
+    btnAdd.classList.add('header__btn-hide');
+
+    document.body.addEventListener('click', closeSearch);
+    library.addEventListener('click', closeSearch);
+  });
+});
+
+
+
+
+
+
